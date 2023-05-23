ec70a24089a8f6e8c0c2ff175f8c53d050db49a2
lukew3 <lukew25073@gmail.com>
2023-05-23 10:55:55 -0400

    get_hn working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// use sqlite;
-// use scraper
 use scraper::{Html, Selector};
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -17,20 +15,35 @@ fn get_hn() {
     let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
     for athing in doc_body.select(&athing) {
         let id = athing.value().attr("id");
-        println!("{}", id.unwrap());
-        let mut db = connection.prepare("SELECT (peak_rank) FROM posts WHERE id=?;").unwrap();
+
+        // Check if the post has already been ranked
+        let mut db = connection.prepare("SELECT peak_rank FROM posts WHERE id=?;").unwrap();
         db.bind((1, id)).unwrap();
         db.next().unwrap();
-        println!("{}", db.read::<String>(0).unwrap());
+        let peak_rank = db.read::<i64, usize>(0).unwrap();
 
+        if peak_rank == 0 {
+            // Add new post to table 
+            db = connection.prepare("INSERT INTO posts (id, peak_rank, peak_time) VALUES (?, ?, ?);").unwrap();
+            db.bind((1, id)).unwrap(); // db.bin((1, id))?;
+            db.bind((2, cur_rank)).unwrap();
+            db.bind((3, time as i64)).unwrap();
+            db.next().unwrap();
+            println!("NEW {} at {}", id.unwrap(), cur_rank);
+        } else if cur_rank < peak_rank {
+            // Update peak_rank in table if cur_rank is better
+            db = connection.prepare("UPDATE posts SET peak_rank=?, peak_time=? WHERE id=?;").unwrap();
+            db.bind((1, cur_rank)).unwrap(); // db.bin((1, id))?;
+            db.bind((2, time as i64)).unwrap();
+            db.bind((3, id)).unwrap();
+            db.next().unwrap();
+            println!("IMPROVED {} from {} -> {}", id.unwrap(), peak_rank, cur_rank);
+        }
 
-        let mut db = connection.prepare("INSERT INTO posts (id, peak_rank, peak_time) VALUES (?, ?, ?);").unwrap();
-        db.bind((1, id)).unwrap(); // db.bin((1, id))?;
-        db.bind((2, cur_rank)).unwrap();
-        db.bind((3, time as i64)).unwrap();
-        db.next().unwrap();
+        // Increment cur_rank counter
         cur_rank += 1;
     }
+    println!("Updated at {}", time);
 }
 
 fn main() {
